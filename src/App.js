60135782b79b51fbf76266eb8269b1bd180f7c49
Ananth Narayanan <ananth@chiselkraft.in>
2020-10-26 15:08:04 +0530

    U5 NavBar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 
 import BaseLayout from "layout/baselayout";
 import PageView from "pages";
+import NavBar from "components/navbar";
 
 class IvecOperatorApp extends React.Component {
   render() {
     return (
       <BaseLayout>
+        {/* render navbar only if logged in */}
+        <NavBar />
         <PageView />
       </BaseLayout>
     );
