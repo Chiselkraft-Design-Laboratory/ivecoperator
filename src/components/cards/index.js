7fa4490e4d4b1e374bd804395bdff3074c877c2d
Ananth Narayanan <ananth@chiselkraft.in>
2020-10-27 11:44:16 +0530

    U6 Dashboard with Cards

---
+++ src/components/cards/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Grid, Paper, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(
+  (theme) => ({
+    root: {
+      padding: theme.spacing(0.5),
+      "& > *": {
+        width: "100%",
+        minHeight: 240,
+        padding: theme.spacing(2),
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "nowrap",
+      },
+    },
+    header: {
+      marginBottom: theme.spacing(2),
+    },
+  }),
+  { index: 1 }
+);
+
+const DashboardCard = ({ mini, rounded, header, children }) => {
+  const cl = useStyles();
+  return (
+    <Grid item xs={10} md={mini ? 4 : 12} classes={{ root: cl.root }}>
+      <Paper square={rounded ? false : true} elevation={4}>
+        {header ? (
+          <Grid item classes={{ root: cl.header }}>
+            {header}
+          </Grid>
+        ) : null}
+        <Grid item xs>
+          {children}
+        </Grid>
+      </Paper>
+    </Grid>
+  );
+};
+
+export default DashboardCard;
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,9 +1,20 @@
 import React from "react";
-import { Grid, makeStyles } from "@material-ui/core";
+import { Grid, makeStyles, Paper } from "@material-ui/core";
+import DashboardCard from "components/cards";
 
 const useStyles = makeStyles(
   (theme) => ({
-    root: {},
+    root: {
+      padding: theme.spacing(1, 3),
+      display: "flex",
+      flexDirection: "row",
+    },
+    grid: {
+      display: "flex",
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyItems: "flex-start",
+    },
   }),
   { index: 1 }
 );
@@ -12,8 +23,22 @@ const DashboardPage = () => {
   const cl = useStyles();
 
   return (
-    <Grid item xs>
-      Dashboard
+    <Grid item xs classes={{ root: cl.root }}>
+      <Grid item xs classes={{ root: cl.grid }}>
+        <DashboardCard mini header="Title">
+          content
+        </DashboardCard>
+        <DashboardCard mini>card</DashboardCard>
+        <DashboardCard mini>card</DashboardCard>
+        <DashboardCard>card</DashboardCard>
+        <DashboardCard>card</DashboardCard>
+        <DashboardCard>card</DashboardCard>
+      </Grid>
+      <Grid item xs={10} md={3} classes={{ root: cl.grid }}>
+        <DashboardCard>card</DashboardCard>
+        <DashboardCard>card</DashboardCard>
+        <DashboardCard>card</DashboardCard>
+      </Grid>
     </Grid>
   );
 };
