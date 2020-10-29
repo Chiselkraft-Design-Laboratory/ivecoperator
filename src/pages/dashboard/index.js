289f2590da06a36bc5323324438c7879ae57a71f
Ananth Narayanan <ananth@chiselkraft.in>
2020-10-29 12:32:47 +0530

    U7 Dashboard cards

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,9 +1,23 @@
 import React from "react";
 import { Grid, makeStyles } from "@material-ui/core";
+import DefaultCard from "components/cards";
 
 const useStyles = makeStyles(
   (theme) => ({
-    root: {},
+    root: {
+      padding: theme.spacing(1, 3),
+      display: "flex",
+      flexDirection: "row",
+
+      justifyContent: "flex-start",
+      alignItems: "flex-start",
+    },
+    grid: {
+      display: "flex",
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: "flex-start",
+    },
   }),
   { index: 1 }
 );
@@ -12,8 +26,26 @@ const DashboardPage = () => {
   const cl = useStyles();
 
   return (
-    <Grid item xs>
-      Dashboard
+    <Grid item xs classes={{ root: cl.root }}>
+      <Grid item xs classes={{ root: cl.grid }}>
+        <DefaultCard mini header="Title">
+          content
+        </DefaultCard>
+        <DefaultCard mini header="Title">
+          content
+        </DefaultCard>
+        <DefaultCard mini header="Title">
+          content
+        </DefaultCard>
+        <DefaultCard>card</DefaultCard>
+        <DefaultCard>card</DefaultCard>
+        <DefaultCard>card</DefaultCard>
+      </Grid>
+      <Grid item xs={10} md={3} classes={{ root: cl.grid }}>
+        <DefaultCard>card</DefaultCard>
+        <DefaultCard>card</DefaultCard>
+        <DefaultCard>card</DefaultCard>
+      </Grid>
     </Grid>
   );
 };
